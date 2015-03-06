c8c0af47bf5c5e590170295b424a60afff7a1854
danigar <daniel.garcia@bq.com>
2015-03-06 18:14:12 +0100

    Some utils like object extends and web reload

---
+++ src/utils.js
@@ -0,0 +1,43 @@
+(function() {
+    //@exclude
+
+    'use strict';
+    /* global Silkroad */
+
+    //@endexclude
+
+
+    /**
+     * A module to some library utils.
+     * @exports validate
+     * @namespace
+     * @memberof app
+     */
+    var utils = Silkroad.utils = {};
+
+    /**
+     * Extend a given object with all the properties in passed-in object(s).
+     * @param  {Object}  obj
+     * @return {Object}
+     */
+    utils.extend = function(obj) {
+        Array.prototype.slice.call(arguments, 1).forEach(function(source) {
+            if (source) {
+                for (var prop in source) {
+                    obj[prop] = source[prop];
+                }
+            }
+        });
+        return obj;
+    };
+
+    utils.reload = function() {
+        // console.log('utils.reload');
+        if (window) {
+            window.location.reload();
+        }
+    };
+
+    return utils;
+
+})();
