bf513bc4e84787f571a8d063acaf2d27973ae7c2
danigar <daniel.garcia@bq.com>
2015-03-06 18:15:37 +0100

    Validate module

--- src/validate.js
+++ src/validate.js
@@ -1,104 +1,113 @@
-'use strict';
-/* global Silkroad */
+(function() {
 
-/**
- * A module to make values validation.
- * @exports validate
- * @namespace
- * @memberof app
- */
-var validate = Silkroad.validate = {};
+    //@exclude
+    'use strict';
+    /* global Silkroad */
+    //@endexclude
 
-/**
- * Checks if some value is not undefined
- * @param  {Mixed}  value
- * @param  {String}  [message]
- * @throws {Error} If return value is false and message are defined
- * @return {Boolean}
- */
-validate.isDefined = function(value, message) {
-    var isUndefined = value === undefined;
 
-    if (isUndefined && message) {
-        throw new Error(message);
-    }
-    return !isUndefined;
-};
+    /**
+     * A module to make values validation.
+     * @exports validate
+     * @namespace
+     * @memberof app
+     */
+    var validate = Silkroad.validate = {};
 
-/**
- * Checks if some value is defined and throw error
- * @param  {Mixed}  value
- * @param  {String}  [message]
- * @throws {Error} If return value is false and message are defined
- * @return {Boolean}
- */
+    /**
+     * Checks if some value is not undefined
+     * @param  {Mixed}  value
+     * @param  {String}  [message]
+     * @throws {Error} If return value is false and message are defined
+     * @return {Boolean}
+     */
+    validate.isDefined = function(value, message) {
+        var isUndefined = value === undefined;
 
-validate.failIfIsDefined = function(value, message) {
-    var isDefined = value !== undefined;
+        if (isUndefined && message) {
+            throw new Error(message);
+        }
+        return !isUndefined;
+    };
 
-    if (isDefined && message) {
-        throw new Error(message);
-    }
-    return !isDefined;
-};
+    /**
+     * Checks if some value is defined and throw error
+     * @param  {Mixed}  value
+     * @param  {String}  [message]
+     * @throws {Error} If return value is false and message are defined
+     * @return {Boolean}
+     */
 
-/**
- * Checks whenever value are null or not
- * @param  {Mixed}  value
- * @param  {String}  [message]
- * @throws {Error} If return value is false and message are defined
- * @return {Boolean}
- */
-validate.isNotNull = function(value, message) {
-    var isNull = value === null;
+    validate.failIfIsDefined = function(value, message) {
+        var isDefined = value !== undefined;
 
-    if (isNull && message) {
-        throw new Error(message);
-    }
-    return !isNull;
-};
+        if (isDefined && message) {
+            throw new Error(message);
+        }
+        return !isDefined;
+    };
 
-/**
- * Checks whenever a value is not null and not undefined
- * @param  {Mixed}  value
- * @param  {String}  [message]
- * @throws {Error} If return value is false and message are defined
- * @return {Boolean}
- */
-validate.isValue = function(value, message) {
-    return this.isDefined(value, message) && this.isNotNull(value, message);
-};
+    /**
+     * Checks whenever value are null or not
+     * @param  {Mixed}  value
+     * @param  {String}  [message]
+     * @throws {Error} If return value is false and message are defined
+     * @return {Boolean}
+     */
+    validate.isNotNull = function(value, message) {
+        var isNull = value === null;
 
-/**
- * Checks whenever a value is greater than other
- * @param  {Mixed}  value
- * @param  {Mixed}  greaterThan
- * @param  {String}  [message]
- * @throws {Error} If return value is false and message are defined
- * @return {Boolean}
- */
-validate.isGreaterThan = function(value, greaterThan, message) {
-    var gt = this.isValue(value) && value > greaterThan;
+        if (isNull && message) {
+            throw new Error(message);
+        }
+        return !isNull;
+    };
 
-    if (!gt && message) {
-        throw new Error(message);
-    }
-    return gt;
-};
+    /**
+     * Checks whenever a value is not null and not undefined
+     * @param  {Mixed}  value
+     * @param  {String}  [message]
+     * @throws {Error} If return value is false and message are defined
+     * @return {Boolean}
+     */
+    validate.isValue = function(value, message) {
+        return this.isDefined(value, message) && this.isNotNull(value, message);
+    };
 
-/**
- * Checks whenever a value is greater or equal than other
- * @param  {Mixed}  value
- * @param  {Mixed} isGreaterThanOrEqual
- * @param  {String}  [message]
- * @throws {Error} If return value is false and message are defined
- * @return {Boolean}
- */
-validate.isGreaterThanOrEqual = function(value, isGreaterThanOrEqual, message) {
-    var gte = this.isValue(value) && value >= isGreaterThanOrEqual;
+    /**
+     * Checks whenever a value is greater than other
+     * @param  {Mixed}  value
+     * @param  {Mixed}  greaterThan
+     * @param  {String}  [message]
+     * @throws {Error} If return value is false and message are defined
+     * @return {Boolean}
+     */
+    validate.isGreaterThan = function(value, greaterThan, message) {
+        var gt = this.isValue(value) && value > greaterThan;
 
-    if (!gte && message) {
-        throw new Error(message);
-    }
-    return gte;
-};
+        if (!gt && message) {
+            throw new Error(message);
+        }
+        return gt;
+    };
+
+    /**
+     * Checks whenever a value is greater or equal than other
+     * @param  {Mixed}  value
+     * @param  {Mixed} isGreaterThanOrEqual
+     * @param  {String}  [message]
+     * @throws {Error} If return value is false and message are defined
+     * @return {Boolean}
+     */
+    validate.isGreaterThanOrEqual = function(value, isGreaterThanOrEqual, message) {
+        var gte = this.isValue(value) && value >= isGreaterThanOrEqual;
+
+        if (!gte && message) {
+            throw new Error(message);
+        }
+        return gte;
+    };
+
+    return validate;
+
+})();
