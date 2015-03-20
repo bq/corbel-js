963028429d1a987d2e431f4912f5afd29c07322a
danigar <daniel.garcia@bq.com>
2015-03-20 15:22:45 +0100

    Refactor services module to inherit from corbel.Object
    Change-Id: I0ba7bde6002851fa169887106cfd262a473e05a0

--- src/services.js
+++ src/services.js
@@ -1,6 +1,11 @@
 //@exclude
+
 'use strict';
+
 /* global corbel */
+
+//deps: [corbel.Object, corbel.Session, corbel.request]
+
 //@endexclude
 
 (function() {
@@ -11,175 +16,156 @@
      * @namespace
      * @memberof corbel
      */
-    var Services = corbel.Services = function(driver) {
-        this.driver = driver;
-    };
+    corbel.Services = corbel.Object.inherit({ //instance props
+        constructor: function(driver) {
+            this.driver = driver;
+        },
+        /**
+         * Execute the actual ajax request.
+         * Retries request with refresh token when credentials are needed.
+         * Refreshes the client when a force update is detected.
+         * Returns a server error (403 - unsupported_version) when force update max retries are reached
+         *
+         * @param  {Promise} dfd     The deferred object to resolve when the ajax request is completed.
+         * @param  {Object} args    The request arguments.
+         */
+        request: function(args) {
 
-    Services.create = function(driver) {
-        return new Services(driver);
-    };
+            var params = this._buildParams(args);
+            return corbel.request.send(params).then(function(response) {
 
-    Services._FORCE_UPDATE_TEXT = 'unsupported_version';
-    Services._FORCE_UPDATE_MAX_RETRIES = 3;
-    // _FORCE_UPDATE_STATUS = 'fu_r';
+                this.driver.session.add(corbel.Services._FORCE_UPDATE_STATUS, 0);
 
-    Services.inherit = corbel.utils.inherit;
+                return Promise.resolve(response);
 
-    /**
-     * Extract a id from the location header of a requestXHR
-     * @param  {Promise} res response from a requestXHR
-     * @return {String}  id from the Location
-     */
-    Services.getLocationId = function(responseObject) {
-        var location;
+            }).catch(function(response) {
 
-        if (responseObject.xhr) {
-            location = arguments[0].xhr.getResponseHeader('location');
-        } else if (responseObject.response.headers.location) {
-            location = responseObject.response.headers.location;
-        }
-        return location ? location.substr(location.lastIndexOf('/') + 1) : undefined;
-    };
+                // Force update
+                if (response.status === 403 &&
+                    response.textStatus === corbel.Services._FORCE_UPDATE_TEXT) {
 
-    /**
-     * Generic Services request.
-     * Support all corbel.request parameters and more:
-     * @param {Object} args
-     * @param {String} [args.method=app.services.method.GET]
-     * @param {String} [args.accessToken] set request with auth. (accessToken overrides args.withAuth)
-     * @param {Boolean} [args.withAuth] set request with auth. (if not exists args.accessToken)
-     * @param {Boolean} [args.noRetry] [Disable automatic retry strategy]
-     * @param {String} [args.retryHook] [reqres hook to retry refresh token]
-     * @return {ES6 Promise}
-     */
-    Services.prototype.request = function(args) {
-        return this.makeRequest(args);
-    };
+                    var retries = this.driver.session.get(corbel.Services._FORCE_UPDATE_STATUS) || 0;
+                    if (retries < corbel.Services._FORCE_UPDATE_MAX_RETRIES) {
+                        retries++;
+                        this.driver.session.add(corbel.Services._FORCE_UPDATE_STATUS, retries);
 
-    /**
-     * Execute the actual ajax request.
-     * Retries request with refresh token when credentials are needed.
-     * Refreshes the client when a force update is detected.
-     * Returns a server error (403 - unsupported_version) when force update max retries are reached
-     *
-     * @param  {Promise} dfd     The deferred object to resolve when the ajax request is completed.
-     * @param  {Object} args    The request arguments.
-     */
-    Services.prototype.makeRequest = function(args) {
+                        corbel.utils.reload(); //TODO nodejs
+                    } else {
 
-        var params = this._buildParams(args);
-        return corbel.request.send(params).then(function(response) {
-
-            // session.add(_FORCE_UPDATE_STATUS, 0); //TODO SESSION
-
-            return Promise.resolve(response);
-
-        }).catch(function(response) {
-            // Force update
-            if (response.status === 403 &&
-                response.textStatus === Services._FORCE_UPDATE_TEXT) {
-
-                var retries = /*session.get(_FORCE_UPDATE_STATUS) ||*/ 0; //TODO SESSION
-                if (retries < Services._FORCE_UPDATE_MAX_RETRIES) {
-                    // console.log('services.request.force_update.reload', retries);
-                    retries++;
-                    // session.add(_FORCE_UPDATE_STATUS, retries); //TODO SESSION
-
-                    // corbel.utils.reload();
+                        // Send an error to the caller
+                        return Promise.reject(response);
+                    }
                 } else {
-                    // console.log('services.request.force_update.fail');
-
-                    // Send an error to the caller
+                    // Any other error fail to the caller
                     return Promise.reject(response);
                 }
-            } else {
-                // Any other error fail to the caller
-                return Promise.reject(response);
+
+            }.bind(this));
+        },
+        /**
+         * Returns a valid corbel.request parameters with default values,
+         * CORS detection and authorization params if needed.
+         * By default, all request are json (dataType/contentType)
+         * with object serialization support
+         * @param  {Object} args
+         * @return {Object}
+         */
+        _buildParams: function(args) {
+
+            // Default values
+            args = args || {};
+
+            args.dataType = args.dataType || 'json';
+            args.contentType = args.contentType || 'application/json; charset=utf-8';
+            args.dataFilter = args.dataFilter || corbel.Services.addEmptyJson;
+
+            // Construct url with query string
+            var url = args.url;
+
+            if (!url) {
+                throw new Error('You must define an url');
             }
 
-        });
-    };
+            if (args.query) {
+                url += '?' + args.query;
+            }
 
-    /**
-     * Returns a valid corbel.request parameters with default values,
-     * CORS detection and authorization params if needed.
-     * By default, all request are json (dataType/contentType)
-     * with object serialization support
-     * @param  {Object} args
-     * @return {Object}
-     */
-    Services.prototype._buildParams = function(args) {
+            var headers = args.headers || {};
 
-        // Default values
-        args = args || {};
+            // @todo: support to oauth token and custom handlers
+            args.accessToken = args.accessToken || this.driver.config.get('IamToken', {}).accessToken;
 
-        args.dataType = args.dataType || 'json';
-        args.contentType = args.contentType || 'application/json; charset=utf-8';
-        args.dataFilter = args.dataFilter || addEmptyJson;
+            // Use access access token if exists
+            if (args.accessToken) {
+                headers.Authorization = 'Bearer ' + args.accessToken;
+            }
+            if (args.noRedirect) {
+                headers['No-Redirect'] = true;
+            }
 
-        // Construct url with query string
-        var url = args.url;
+            headers.Accept = 'application/json';
+            if (args.Accept) {
+                headers.Accept = args.Accept;
+                args.dataType = undefined; // Accept & dataType are incompatibles
+            }
 
-        if (!url) {
-            throw new Error('You must define an url');
+            var params = {
+                url: url,
+                dataType: args.dataType,
+                contentType: args.contentType,
+                method: args.method || corbel.request.method.GET,
+                headers: headers,
+                data: (args.contentType.indexOf('json') !== -1 && typeof args.data === 'object' ? JSON.stringify(args.data) : args.data),
+                dataFilter: args.dataFilter
+            };
+
+            // For binary requests like 'blob' or 'arraybuffer', set correct dataType
+            params.dataType = args.binaryType || params.dataType;
+
+            // Prevent JQuery to proceess 'blob' || 'arraybuffer' data
+            // if ((params.dataType === 'blob' || params.dataType === 'arraybuffer') && (params.method === 'PUT' || params.method === 'POST')) {
+            //     params.processData = false;
+            // }
+
+            // console.log('services._buildParams (params)', params);
+            // if (args.data) {
+            //      console.log('services._buildParams (data)', args.data);
+            // }
+
+            return params;
+        },
+
+    }, { //Static props
+        _FORCE_UPDATE_TEXT: 'unsupported_version',
+        _FORCE_UPDATE_MAX_RETRIES: 3,
+        _FORCE_UPDATE_STATUS: 'fu_r',
+        create: function(driver) {
+            return new corbel.Services(driver);
+        },
+        /**
+         * Extract a id from the location header of a requestXHR
+         * @param  {Promise} res response from a requestXHR
+         * @return {String}  id from the Location
+         */
+        getLocationId: function(responseObject) {
+            var location;
+
+            if (responseObject.xhr) {
+                location = arguments[0].xhr.getResponseHeader('location');
+            } else if (responseObject.response.headers.location) {
+                location = responseObject.response.headers.location;
+            }
+            return location ? location.substr(location.lastIndexOf('/') + 1) : undefined;
+        },
+        addEmptyJson: function(response, type) {
+            if (!response && type === 'json') {
+                response = '{}';
+            }
+            return response;
         }
+    });
 
-        if (args.query) {
-            url += '?' + args.query;
-        }
 
-        var headers = args.headers || {};
-
-        // @todo: support to oauth token and custom handlers
-        args.accessToken = args.accessToken || this.driver.config.get('IamToken', {}).accessToken;
-
-        // Use access access token if exists
-        if (args.accessToken) {
-            headers.Authorization = 'Bearer ' + args.accessToken;
-        }
-        if (args.noRedirect) {
-            headers['No-Redirect'] = true;
-        }
-
-        headers.Accept = 'application/json';
-        if (args.Accept) {
-            headers.Accept = args.Accept;
-            args.dataType = undefined; // Accept & dataType are incompatibles
-        }
-
-        var params = {
-            url: url,
-            dataType: args.dataType,
-            contentType: args.contentType,
-            method: args.method || corbel.request.method.GET,
-            headers: headers,
-            data: (args.contentType.indexOf('json') !== -1 && typeof args.data === 'object' ? JSON.stringify(args.data) : args.data),
-            dataFilter: args.dataFilter
-        };
-
-        // For binary requests like 'blob' or 'arraybuffer', set correct dataType
-        params.dataType = args.binaryType || params.dataType;
-
-        // Prevent JQuery to proceess 'blob' || 'arraybuffer' data
-        // if ((params.dataType === 'blob' || params.dataType === 'arraybuffer') && (params.method === 'PUT' || params.method === 'POST')) {
-        //     params.processData = false;
-        // }
-
-        // console.log('services._buildParams (params)', params);
-        // if (args.data) {
-        //      console.log('services._buildParams (data)', args.data);
-        // }
-
-        return params;
-    };
-
-    var addEmptyJson = function(response, type) {
-        if (!response && type === 'json') {
-            response = '{}';
-        }
-        return response;
-    };
-
-    return Services;
+    return corbel.Services;
 
 })();
