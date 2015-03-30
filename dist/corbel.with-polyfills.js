6808e7f894925785da2aa4310ff45e9c1598a487
Anthanh <anthanh.pham@bq.com>
2015-03-30 17:41:11 +0200

    test fix
    Change-Id: Ic289bd284db1932219dac67d05357d0b928db370

--- dist/corbel.with-polyfills.js
+++ dist/corbel.with-polyfills.js
@@ -998,35 +998,33 @@
     
         corbel.CorbelDriver = CorbelDriver;
     
+        /**
+         * Instanciates new corbel driver
+         * @param {Object} config
+         * @param {String} config.urlBase
+         * @param {String} [config.urlBase]
+         * @param {String} [config.clientId]
+         * @param {String} [config.clientSecret]
+         * @param {String} [config.scopesApp]
+         * @param {String} [config.scopesUserLogin]
+         * @param {String} [config.scopesUserCreate]
+         * @param {String} [config.resourcesEndpoint]
+         * @param {String} [config.iamEndpoint]
+         * @param {String} [config.evciEndpoint]
+         * @param {String} [config.oauthEndpoint]
+         * @param {String} [config.oauthClientId]
+         * @param {String} [config.oauthSecret]
+         * @return {CorbelDriver}
+         */
         corbel.getDriver = function(config) {
             config = config || {};
-    
-            // var keys = [
-            //     'urlBase',
-            //     'clientId',
-            //     'clientSecret',
-            //     'scopesApp',
-            //     'scopesUserLogin',
-            //     'scopesUserCreate',
-            //     'resourcesEndpoint',
-            //     'iamEndpoint',
-            //     'evciEndpoint',
-            //     'oauthEndpoint',
-            //     'oauthClientId',
-            //     'oauthSecret'
-            // ];
-    
-            // keys.forEach(function(key) {
-            //     if (!config[key]) {
-            //         throw new Error('undefined:' + key);
-            //     }
-            // });
     
             return new CorbelDriver(config);
         };
     
     
     })();
+    
     (function() {
     
         /**
