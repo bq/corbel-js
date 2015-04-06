10305d1f3680fb441ac32d3e16d7f5fc4a8afbef
danigar <daniel.garcia@bq.com>
2015-04-06 13:27:25 +0200

    Resources refactor to support query params
    Change-Id: I606f26b91dc433298d0580071a90b09f32f3eab3

--- src/resources/relation.js
+++ src/resources/relation.js
@@ -4,6 +4,7 @@
      /*globals corbel */
      //@endexclude
 
+
      /**
       * Relation
       * @class
@@ -13,59 +14,67 @@
       * @param  {String} destType    The destination resource type
       */
      corbel.Resources.Relation = corbel.Resources.ResourceBase.inherit({
-         constructor: function(srcType, srcId, destType, driver) {
+         constructor: function(srcType, srcId, destType, driver, params) {
              this.type = srcType;
              this.srcId = srcId;
              this.destType = destType;
              this.driver = driver;
+             this.params = params || {};
          },
          /**
           * Gets the resources of a relation
           * @method
-          * @memberOf resources.RelationBuilder
+          * @memberOf Resources.Relation
           * @param  {String} dataType    Mime type of the expected resource
           * @param  {String} destId         Relationed resource
           * @param  {Object} params      Params of the silkroad request
           * @return {Promise}            ES6 promise that resolves to a relation {Object} or rejects with a {@link SilkRoadError}
           * @see {@link corbel.util.serializeParams} to see a example of the params
           */
-         get: function(dataType, destId, params) {
-             // console.log('resourceInterface.relation.get', params);
-             return this.request({
+         get: function(destId, options) {
+             options = this.getDefaultOptions(options);
+
+             var args = corbel.utils.extend(options, {
                  url: this.buildUri(this.type, this.srcId, this.destType, destId),
                  method: corbel.request.method.GET,
-                 query: params ? corbel.util.serializeParams(params) : null,
-                 Accept: dataType
+                 Accept: options.dataType
              });
+
+             return this.request(args);
          },
          /**
           * Adds a new relation between Resources
           * @method
-          * @memberOf Resources.RelationBuilder
+          * @memberOf Resources.Relation
           * @param  {String} destId          Relationed resource
           * @param  {Object} relationData Additional data to be added to the relation (in json)
           * @return {Promise}             ES6 promise that resolves to undefined (void) or rejects with a {@link SilkRoadError}
           * @example uri = '555'
           */
-         add: function(destId, relationData) {
-             // console.log('resourceInterface.relation.add', relationData);
-             return this.request({
+         add: function(destId, relationData, options) {
+             options = this.getDefaultOptions(options);
+
+             var args = corbel.utils.extend(options, {
                  url: this.buildUri(this.type, this.srcId, this.destType, destId),
                  contentType: 'application/json',
                  data: relationData,
                  method: corbel.request.method.PUT
              });
+
+             return this.request(args);
          },
          /**
           * Adds a new relation between Resources
           * @method
-          * @memberOf Resources.RelationBuilder
+          * @memberOf Resources.Relation
           * @param  {Integer} pos          The new position
           * @return {Promise}              ES6 promise that resolves to undefined (void) or rejects with a {@link SilkRoadError}
           */
-         move: function(destId, pos) {
-             // console.log('resourceInterface.relation.move', pos);
-             return this.request({
+         move: function(destId, pos, options) {
+
+             options = this.getDefaultOptions(options);
+
+             var args = corbel.utils.extend(options, {
                  url: this.buildUri(this.type, this.srcId, this.destType, destId),
                  contentType: 'application/json',
                  data: {
@@ -73,22 +82,27 @@
                  },
                  method: corbel.request.method.PUT
              });
+
+             return this.request(args);
          },
          /**
           * Deletes a relation between Resources
           * @method
-          * @memberOf Resources.RelationBuilder
+          * @memberOf Resources.Relation
           * @param  {String} destId          Relationed resource
           * @return {Promise}                ES6 promise that resolves to undefined (void) or rejects with a {@link SilkRoadError}
           * @example
           * destId = 'music:Track/555'
           */
-         delete: function(destId) {
-             // console.log('resourceInterface.relation.delete', destId);
-             return this.request({
+         delete: function(destId, options) {
+             options = this.getDefaultOptions(options);
+
+             var args = corbel.utils.extend(options, {
                  url: this.buildUri(this.type, this.srcId, this.destType, destId),
                  method: corbel.request.method.DELETE
              });
+
+             return this.request(args);
          }
      });
 
