c03890bd2456f4aa7681b98c4a693e3e6c323ad6
danigar <daniel.garcia@bq.com>
2015-04-06 13:27:25 +0200

    Resources refactor to support query params
    Change-Id: I606f26b91dc433298d0580071a90b09f32f3eab3

--- src/resources/resource.js
+++ src/resources/resource.js
@@ -13,69 +13,80 @@
      */
     corbel.Resources.Resource = corbel.Resources.ResourceBase.inherit({
 
-        constructor: function(type, id, driver) {
+        constructor: function(type, id, driver, params) {
             this.type = type;
             this.id = id;
             this.driver = driver;
+            this.params = params || {};
         },
 
         /**
          * Gets a resource
          * @method
-         * @memberOf resources.ResourceBuilder
-         * @param  {String} [dataType="application/json"]                Mime type of the expected resource
-         * @param  {Object} [params]                Additional request parameters
-         * @param  {String} [params.binaryType]     XMLHttpRequest 2 responseType value ('blob'|'arraybuffer'|undefined)
+         * @memberOf resources.Resource
+         * @param  {Object} options
+         * @param  {String} [options.dataType]      Mime type of the expected resource
+         * @param  {Object} [options.params]        Additional request parameters
          * @return {Promise}                        ES6 promise that resolves to a Resource {Object} or rejects with a {@link SilkRoadError}
          * @see {@link services.request} to see a example of the params
          */
-        get: function(dataType, params) {
-            // console.log('resourceInterface.resource.get', dataType);
-            var args = params || {};
-            args.url = this.buildUri(this.type, this.id);
-            args.method = corbel.request.method.GET;
-            args.contentType = dataType;
-            args.Accept = dataType;
+        get: function(options) {
+            options = this.getDefaultOptions(options);
+
+            var args = corbel.utils.extend(options, {
+                url: this.buildUri(this.type, this.id),
+                method: corbel.request.method.GET,
+                contentType: options.dataType,
+                Accept: options.dataType
+            });
+
             return this.request(args);
         },
 
         /**
          * Updates a resource
          * @method
-         * @memberOf resources.ResourceBuilder
+         * @memberOf resources.Resource
          * @param  {Object} data                    Data to be updated
-         * @param  {Object} [params]                Additional request parameters
-         * @param  {String} [params.binaryType]     XMLHttpRequest 2 content type value for the sending content ('blob'|'arraybuffer'|undefined)
-         * @param  {String} dataType                Mime tipe of the sent data
+         * @param  {Object} options
+         * @param  {String} [options.dataType]      Mime tipe of the sent data
+         * @param  {Object} [options.params]        Additional request parameters
          * @return {Promise}                        ES6 promise that resolves to undefined (void) or rejects with a {@link SilkRoadError}
          * @see {@link services.request} to see a example of the params
          */
-        update: function(dataType, data, params) {
-            // console.log('resourceInterface.resource', data);
-            var args = params || {};
-            args.url = this.buildUri(this.type, this.id);
-            args.method = corbel.request.method.PUT;
-            args.data = data;
-            args.contentType = dataType;
-            args.Accept = dataType;
+        update: function(data, options) {
+            options = this.getDefaultOptions(options);
+
+            var args = corbel.utils.extend(options, {
+                url: this.buildUri(this.type, this.id),
+                method: corbel.request.method.GET,
+                data: data,
+                contentType: options.dataType,
+                Accept: options.dataType
+            });
+
             return this.request(args);
         },
 
         /**
          * Deletes a resource
          * @method
-         * @memberOf Resources.ResourceBuilder
-         * @param  {String} dataType    Mime tipe of the delete data
-         * @return {Promise}            ES6 promise that resolves to undefined (void) or rejects with a {@link SilkRoadError}
+         * @memberOf resources.Resource
+         * @param  {Object} options
+         * @param  {Object} [options.dataType]      Mime tipe of the delete data
+         * @return {Promise}                        ES6 promise that resolves to undefined (void) or rejects with a {@link SilkRoadError}
          */
-        delete: function(dataType) {
-            // console.log('resourceInterface.resource.delete');
-            return this.request({
+        delete: function(options) {
+            options = this.getDefaultOptions(options);
+
+            var args = corbel.utils.extend(options, {
                 url: this.buildUri(this.type, this.id),
                 method: corbel.request.method.DELETE,
-                contentType: dataType,
-                Accept: dataType
+                contentType: options.dataType,
+                Accept: options.dataType
             });
+
+            return this.request(args);
         }
 
     });
