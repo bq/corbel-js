98443f66cfeb790242abbf94e9058681fafa7204
Jenkins <dev@bq.com>
2015-05-14 14:53:15 +0000

    Release v0.0.6

--- dist/corbel.js
+++ dist/corbel.js
@@ -35,6 +35,7 @@
             // create isntance modules with injected driver
             this.iam = corbel.Iam.create(this);
             this.resources = corbel.Resources.create(this);
+            this.assets = corbel.Assets.create(this);
             this.services = corbel.Services.create(this);
             this.session = corbel.Session.create(this);
         }
@@ -1640,7 +1641,26 @@
                     serialize = root.atob;
                 }
                 var decoded = assertion.split('.');
-                return [JSON.parse(serialize(decoded[0])), JSON.parse(serialize(decoded[1]))];
+
+                try {
+                    decoded[0] = JSON.parse(serialize(decoded[0]));
+                } catch (e) {
+                    console.log('error:jwt:decode:0');
+                    decoded[0] = {};
+                }
+
+                try {
+                    decoded[1] = JSON.parse(serialize(decoded[1]));
+                } catch (e) {
+                    console.log('error:jwt:decode:1');
+                    decoded[1] = {};
+                }
+
+                Object.keys(decoded[1]).forEach(function(key) {
+                    decoded[0][key] = decoded[1][key];
+                });
+
+                return decoded[0];
             }
 
         };
@@ -2802,6 +2822,152 @@
         return corbel.requestParamsBuilder;
 
     })(aggregationBuilder, queryBuilder, sortBuilder, pageBuilder);
+    (function() {
+        corbel.Assets = corbel.Object.inherit({
+
+            /**
+             * Creates a new AssetsBuilder
+             * @param  {String} id String with the asset id or 'all' key
+             * @return {Assets}
+             */
+            constructor: function(driver) {
+                this.driver = driver;
+
+                return function(id) {
+                    var builder = new corbel.Assets.AssetsBuilder(id);
+                    builder.driver = driver;
+                    return builder;
+                };
+            },
+
+
+        }, {
+
+            moduleName: 'assets',
+
+            create: function(driver) {
+                return new corbel.Assets(driver);
+            }
+
+        });
+
+        return corbel.Assets;
+
+    })();
+
+    (function() {
+
+        var AssetsBuilder = corbel.Assets.AssetsBuilder = corbel.Services.BaseServices.inherit({
+
+            /**
+             * Creates a new AssetsBuilder
+             * @param  {String} id String with the asset id or 'all' key
+             * @return {Assets}
+             */
+            constructor: function(id) {
+                this.uri = 'asset';
+                this.id = id;
+            },
+
+            /**
+             * Gets my user assets
+             * @method
+             * @memberOf assets.AssetBuilder
+             * @param  {Object} [params]      Params of the silkroad request
+             * @return {Promise}              Promise that resolves to a Asset {Object} or rejects with a {@link SilkRoadError}
+             */
+            get: function(params) {
+                return this.request({
+                    url: this._buildUri(this.uri, this.id),
+                    method: corbel.request.method.GET,
+                    query: params ? corbel.utils.serializeParams(params) : null
+                });
+            },
+
+            /**
+             * Delete asset
+             * @method
+             * @memberOf assets.AssetBuilder
+             * @return {Promise}        Promise that resolves to undefined (void) or rejects with a {@link SilkRoadError}
+             */
+            delete: function() {
+                return this.request({
+                    url: this._buildUri(this.uri, this.id),
+                    method: corbel.request.method.DELETE
+                });
+            },
+
+            /**
+             * Creates a new asset
+             * @method
+             * @memberOf assets.AssetBuilder
+             * @param {Object} data                Contains the data of the new asset
+             * @param {String} userId The user id
+             * @param {String} name The asset name
+             * @param {Date} expire Expire date
+             * @param {Boolean} active If asset is active
+             * @param {Array} scopes Scopes of the asset
+             * @return {Promise}                    Promise that resolves in the new asset id or rejects with a {@link SilkRoadError}
+             */
+            create: function(data) {
+                return this.request({
+                    url: this._buildUri(this.uri),
+                    method: corbel.request.method.POST,
+                    data: data
+                }).
+                then(function(res) {
+                    return corbel.Services.getLocationId(res);
+                });
+            },
+
+            /**
+             * Generates a JWT that contains the scopes of the actual user's assets and redirects to iam to upgrade user's token
+             * @method
+             * @memberOf assets.AssetBuilder
+             * @return {Promise} Promise that resolves to a redirection to iam/oauth/token/upgrade or rejects with a {@link SilkRoadError}
+             */
+            access: function(params) {
+                var args = params || {};
+                args.url = this._buildUri(this.uri + '/access');
+                args.method = corbel.request.method.GET;
+                args.noRedirect = true;
+
+                var that = this;
+                return that.request(args).then(function(uri) {
+                    return that.request({
+                        noRetry: args.noRetry,
+                        url: uri
+                    });
+                });
+            },
+
+            _buildUri: function(path, id) {
+                var uri = '',
+                    urlBase = this.driver.config.get('assetsEndpoint', null) ?
+                    this.driver.config.get('assetsEndpoint') :
+                    this.driver.config.get('urlBase').replace(corbel.Config.URL_BASE_PLACEHOLDER, corbel.Assets.moduleName);
+
+                uri = urlBase + path;
+                if (id) {
+                    uri += '/' + id;
+                }
+                return uri;
+            }
+
+        }, {
+
+            moduleName: 'assets',
+
+            create: function(driver) {
+                return new corbel.AssetsBuilder(driver);
+            }
+
+        });
+
+        return AssetsBuilder;
+
+    })();
+
     (function() {
         corbel.Resources = corbel.Object.inherit({
 
