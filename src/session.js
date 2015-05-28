155862a320b9a0b0e0e97bedc1c272a82af36721
Anthanh <anthanh.pham@bq.com>
2015-05-28 19:35:40 +0200

    removeDir included in destroy in node environment
    Change-Id: Iaae240840dbd0f7561300db27f4e1a0556b8ac98

--- src/session.js
+++ src/session.js
@@ -12,222 +12,216 @@
 
 (function() {
 
+  /**
+   * A module to manage session data.
+   * @exports session
+   * @namespace
+   * @memberof corbel
+   */
+  corbel.Session = corbel.Object.inherit({
+    constructor: function(driver) {
+      this.driver = driver;
+      this.status = '';
+      //Set localStorage in node-js enviroment
+      if (corbel.Config.isNode) {
+        var LocalStorage = require('node-localstorage').LocalStorage,
+          fs = require('fs');
+
+        if (fs.existsSync(corbel.Session.SESSION_PATH_DIR) === false) {
+          fs.mkdirSync(corbel.Session.SESSION_PATH_DIR);
+        }
+
+        if (fs.existsSync(corbel.Session.SESSION_PATH_DIR + '/' + this.driver.guid) === false) {
+          fs.mkdirSync(corbel.Session.SESSION_PATH_DIR + '/' + this.driver.guid);
+        }
+
+        this.localStorage = new LocalStorage(corbel.Session.SESSION_PATH_DIR + '/' + this.driver.guid);
+
+        process.once('exit', function() {
+          // if (this.isPersistent() === false) {
+          this.destroy();
+          // }
+        }.bind(this));
+
+      } else {
+        this.localStorage = localStorage;
+      }
+
+      //Set sessionStorage in node-js enviroment
+      if (typeof sessionStorage === 'undefined' || sessionStorage === null) {
+        this.sessionStorage = this.localStorage;
+      } else {
+        this.sessionStorage = sessionStorage;
+      }
+    },
+
     /**
-     * A module to manage session data.
-     * @exports session
-     * @namespace
-     * @memberof corbel
+     * Gets a specific session value
+     * @param  {String} key
+     * @return {String|Number|Boolean}
      */
-    corbel.Session = corbel.Object.inherit({
-        constructor: function(driver) {
-            this.driver = driver;
-            this.status = '';
-            //Set localStorage in node-js enviroment
-            if (corbel.Config.isNode) {
-                var LocalStorage = require('node-localstorage').LocalStorage,
-                    fs = require('fs');
+    get: function(key) {
 
-                if (fs.existsSync(corbel.Session.SESSION_PATH_DIR) === false) {
-                    fs.mkdirSync(corbel.Session.SESSION_PATH_DIR);
-                }
+      key = key || 'session';
 
-                if (fs.existsSync(corbel.Session.SESSION_PATH_DIR + '/' + this.driver.guid) === false) {
-                    fs.mkdirSync(corbel.Session.SESSION_PATH_DIR + '/' + this.driver.guid);
-                }
+      var storage = this.localStorage;
+      if (!this.isPersistent()) {
+        storage = this.sessionStorage;
+      }
 
-                this.localStorage = new LocalStorage(corbel.Session.SESSION_PATH_DIR + '/' + this.driver.guid);
+      try {
+        return JSON.parse(storage.getItem(key));
+      } catch (e) {
+        return storage.getItem(key);
+      }
+    },
 
-                process.once('exit', function() {
-                    // if (this.isPersistent() === false) {
-                    this.destroy();
-                    this.removeDir();
-                    // }
-                }.bind(this));
+    /**
+     * Adds a key-value in the user session
+     * @param {String} key
+     * @param {String|Number|Boolean} value
+     * @param {Boolean} [forcePersistent] Force to save value in localStorage
+     */
+    add: function(key, value, forcePersistent) {
+      var storage = this.sessionStorage;
 
-            } else {
-                this.localStorage = localStorage;
-            }
+      if (this.isPersistent() || forcePersistent) {
+        storage = localStorage;
+      }
 
-            //Set sessionStorage in node-js enviroment
-            if (typeof sessionStorage === 'undefined' || sessionStorage === null) {
-                this.sessionStorage = this.localStorage;
-            } else {
-                this.sessionStorage = sessionStorage;
-            }
-        },
+      if (corbel.validate.isObject(value)) {
+        value = JSON.stringify(value);
+      }
+      if (value === undefined) {
+        storage.removeItem(key);
+      } else {
+        storage.setItem(key, value);
+      }
+    },
 
-        /**
-         * Gets a specific session value
-         * @param  {String} key
-         * @return {String|Number|Boolean}
-         */
-        get: function(key) {
+    /**
+     * Checks active sessions and updates the app status
+     * @return {Boolean}
+     */
+    gatekeeper: function() {
+      var exist = this.exist();
 
-            key = key || 'session';
+      if (exist) {
+        this.setStatus('logged', true);
+      } else {
+        this.setStatus('logged', false);
+      }
 
-            var storage = this.localStorage;
-            if (!this.isPersistent()) {
-                storage = this.sessionStorage;
-            }
+      return exist;
+    },
 
-            try {
-                return JSON.parse(storage.getItem(key));
-            } catch (e) {
-                return storage.getItem(key);
-            }
-        },
+    /**
+     * Checks when a session exists
+     * @return {Boolean}
+     */
+    exist: function() {
+      // TODO: Do it better, diff between anonymous and real user
+      // Setted at user.login()
+      return this.get('loggedTime') ? true : false;
+    },
 
-        /**
-         * Adds a key-value in the user session
-         * @param {String} key
-         * @param {String|Number|Boolean} value
-         * @param {Boolean} [forcePersistent] Force to save value in localStorage
-         */
-        add: function(key, value, forcePersistent) {
-            var storage = this.sessionStorage;
+    /**
+     * Creates a user session data
+     * @param  {Object} args
+     * @param  {Boolean} args.persistent
+     * @param  {String} args.accessToken
+     * @param  {String} args.oauthService
+     * @param  {Object} args.user
+     */
+    logged: function(args) {
+      corbel.validate.isValue(args.accessToken, 'Missing accessToken');
+      corbel.validate.isValue(args.refreshToken, 'Missing refreshToken');
+      corbel.validate.isValue(args.expiresAt, 'Missing expiresAt');
+      corbel.validate.isValue(args.user, 'Missing user');
+      corbel.validate.isValue(args.oauthService || args.loginBasic, 'Missing oauthService and loginBasic');
 
-            if (this.isPersistent() || forcePersistent) {
-                storage = localStorage;
-            }
+      this.setPersistent(args.persistent);
 
-            if (corbel.validate.isObject(value)) {
-                value = JSON.stringify(value);
-            }
-            if (value === undefined) {
-                storage.removeItem(key);
-            } else {
-                storage.setItem(key, value);
-            }
-        },
+      this.add('accessToken', args.accessToken);
+      this.add('refreshToken', args.refreshToken);
+      this.add('expiresAt', args.expiresAt);
+      this.add('oauthService', args.oauthService);
+      this.add('loginBasic', args.loginBasic);
+      this.add('loggedTime', new Date().getTime());
+      this.add('user', args.user);
 
-        /**
-         * Checks active sessions and updates the app status
-         * @return {Boolean}
-         */
-        gatekeeper: function() {
-            var exist = this.exist();
+    },
 
-            if (exist) {
-                this.setStatus('logged', true);
-            } else {
-                this.setStatus('logged', false);
-            }
+    /**
+     * Proxy call for session.add(key, undefined)
+     * @param  {String} key
+     */
+    remove: function(key) {
+      this.add(key);
+    },
 
-            return exist;
-        },
+    /**
+     * Checks if the current session is persistent or not
+     * @return {Boolean}
+     */
+    isPersistent: function() {
+      return (this.localStorage.persistent ? true : false);
+    },
 
-        /**
-         * Checks when a session exists
-         * @return {Boolean}
-         */
-        exist: function() {
-            // TODO: Do it better, diff between anonymous and real user
-            // Setted at user.login()
-            return this.get('loggedTime') ? true : false;
-        },
+    /**
+     * Creates a user session with
+     * @param {Boolean} persistent
+     */
+    setPersistent: function(persistent) {
+      if (persistent) {
+        this.localStorage.setItem('persistent', persistent);
+      } else {
+        this.localStorage.removeItem('persistent');
+      }
+    },
 
-        /**
-         * Creates a user session data
-         * @param  {Object} args
-         * @param  {Boolean} args.persistent
-         * @param  {String} args.accessToken
-         * @param  {String} args.oauthService
-         * @param  {Object} args.user
-         */
-        logged: function(args) {
-            corbel.validate.isValue(args.accessToken, 'Missing accessToken');
-            corbel.validate.isValue(args.refreshToken, 'Missing refreshToken');
-            corbel.validate.isValue(args.expiresAt, 'Missing expiresAt');
-            corbel.validate.isValue(args.user, 'Missing user');
-            corbel.validate.isValue(args.oauthService || args.loginBasic, 'Missing oauthService and loginBasic');
+    /**
+     * Move a session value to a persistent value (if exists)
+     * @param  {String} name
+     */
+    persist: function(name) {
+      var value = this.sessionStorage.getItem(name);
+      if (value) {
+        this.localStorage.setItem(name, value);
+        this.sessionStorage.removeItem(name);
+      }
+    },
 
-            this.setPersistent(args.persistent);
-
-            this.add('accessToken', args.accessToken);
-            this.add('refreshToken', args.refreshToken);
-            this.add('expiresAt', args.expiresAt);
-            this.add('oauthService', args.oauthService);
-            this.add('loginBasic', args.loginBasic);
-            this.add('loggedTime', new Date().getTime());
-            this.add('user', args.user);
-
-        },
-
-        /**
-         * Proxy call for session.add(key, undefined)
-         * @since 1.6.0
-         * @param  {String} key
-         */
-        remove: function(key) {
-            this.add(key);
-        },
-
-        removeDir: function() {
-            if ( /*corbel.enviroment === 'node'*/ typeof module !== 'undefined' && module.exports) {
-                var fs = require('fs');
-                try {
-                    fs.rmdirSync(corbel.Session.SESSION_PATH_DIR + '/' + this.driver.guid);
-                } catch (ex) {}
-            }
-        },
-
-        /**
-         * Checks if the current session is persistent or not
-         * @return {Boolean}
-         */
-        isPersistent: function() {
-            return (this.localStorage.persistent ? true : false);
-        },
-
-        /**
-         * Creates a user session with
-         * @param {Boolean} persistent
-         */
-        setPersistent: function(persistent) {
-            if (persistent) {
-                this.localStorage.setItem('persistent', persistent);
-            } else {
-                this.localStorage.removeItem('persistent');
-            }
-        },
-
-        /**
-         * Move a session value to a persistent value (if exists)
-         * @param  {String} name
-         */
-        persist: function(name) {
-            var value = this.sessionStorage.getItem(name);
-            if (value) {
-                this.localStorage.setItem(name, value);
-                this.sessionStorage.removeItem(name);
-            }
-        },
-
-        /**
-         * Clears all user storage and remove storage dir for nodejs /*
-         */
-        destroy: function() {
-            this.localStorage.clear();
-            if ( /*corbel.enviroment === 'node'*/ typeof module !== 'undefined' && module.exports) {
-
-            } else {
-                this.sessionStorage.clear();
-            }
-
+    /**
+     * Clears all user storage and remove storage dir for nodejs /*
+     */
+    destroy: function() {
+      if (this.localStorage) {
+        this.localStorage.clear();
+        if (corbel.Config.isNode) {
+          var fs = require('fs');
+          try {
+            fs.rmdirSync(corbel.Session.SESSION_PATH_DIR + '/' + this.driver.guid);
+          } catch (ex) {}
+        } else {
+          this.sessionStorage.clear();
         }
-    }, {
-        SESSION_PATH_DIR: './.storage',
+        this.localStorage = undefined;
+      }
+    }
+  }, {
+    SESSION_PATH_DIR: './.storage',
 
-        /**
-         * Static factory method for session object
-         * @param  {corbel.Driver} corbel-js driver
-         * @return {corbel.session}
-         */
-        create: function(driver) {
-            return new corbel.Session(driver);
-        }
-    });
+    /**
+     * Static factory method for session object
+     * @param  {corbel.Driver} corbel-js driver
+     * @return {corbel.session}
+     */
+    create: function(driver) {
+      return new corbel.Session(driver);
+    }
+  });
 
-    return corbel.Session;
+  return corbel.Session;
 
 })();
