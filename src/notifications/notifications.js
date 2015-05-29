4c904806a86f8d6dea84452cace4d6028621d04f
laurasada <laura.sada@bq.com>
2015-05-29 16:23:01 +0200

    Migrate Notifications module from corejs-base to Corbel-js
    Change-Id: I653ffa59374e2bde85cf00c53c0a8609842725cf

---
+++ src/notifications/notifications.js
@@ -0,0 +1,37 @@
+(function() {
+    //@exclude
+    'use strict';
+    /*globals corbel */
+    //@endexclude
+
+    corbel.Notifications = corbel.Object.inherit({
+
+        /**
+         * Creates a new NotificationsBuilder
+         * @param  {String} id String with the asset id or 'all' key
+         * @return {Notifications}
+         */
+        constructor: function(driver) {
+            this.driver = driver;
+
+            return function(id) {
+                var builder = new corbel.Notifications.NotificationsBuilder(id);
+                builder.driver = driver;
+                return builder;
+            };
+        },
+
+
+    }, {
+
+        moduleName: 'notifications',
+
+        create: function(driver) {
+            return new corbel.Notifications(driver);
+        }
+
+    });
+
+    return corbel.Notifications;
+
+})();
