750b33efa03cf22bee43e81fd9226c1b978e6ee2
Rafael Pedrola <rafinskipg@gmail.com>
2015-06-11 12:15:56 +0200

    Added some changes
    Change-Id: Iaf4080340979e879ae0df4e4687b076d9eb4b1c7

--- src/services/base-services.js
+++ src/services/base-services.js
@@ -10,6 +10,27 @@
 //@endexclude
 
 var BaseServices = (function() {
+
+    function buildDefaults(destiny, defaults){
+      Object.keys(defaults).forEach(function(key){
+        if(typeof(destiny[key]) === 'undefined'){
+          destiny[key] = defaults[key];
+        }
+      });
+
+      return destiny;
+    }
+
+    function pick(object, keys){
+      var destiny = {};
+
+      keys.forEach(function(key){
+        destiny[key] = object[key];
+      });
+
+      return destiny;
+    }
+
     /**
      * A base object to inherit from for make corbel-js requests with custom behavior.
      * @exports corbel.ServicesBase
@@ -32,6 +53,42 @@ var BaseServices = (function() {
         request: function(args) {
 
             var params = this._buildParams(args);
+
+            var isJson = function(item){
+              return typeof(item) === 'string' && item.indexOf('{') === 0;
+            };
+
+            var assignCaller = function(item){
+              if(isJson(item)){
+                try{
+                  var objectParsed = JSON.parse(item);
+                  objectParsed.caller = params.caller;
+                  item = JSON.stringify(objectParsed);
+                }catch(e){
+                  console.error(e);
+                }
+              }
+
+              return item;
+            };
+
+            var assignResponseCallers = function(response){
+              // Any other error fail to the caller
+              if(params.caller && response && response.data){
+                //Avoid read only states
+                response.data = _.clone(response.data);
+
+                if(response.data.response){
+                  response.data.response = assignCaller(response.data.response);
+                }
+
+                if(response.data.responseText){
+                  response.data.responseText = assignCaller(response.data.responseText);
+                }
+              }
+            };
+
+
             return corbel.request.send(params).then(function(response) {
 
                 this.driver.config.set(corbel.Services._FORCE_UPDATE_STATUS, 0);
@@ -53,10 +110,11 @@ var BaseServices = (function() {
                     } else {
 
                         // Send an error to the caller
+                        assignResponseCallers(response);
                         return Promise.reject(response);
                     }
                 } else {
-                    // Any other error fail to the caller
+                    assignResponseCallers(response);
                     return Promise.reject(response);
                 }
 
@@ -73,54 +131,45 @@ var BaseServices = (function() {
         _buildParams: function(args) {
 
             // Default values
-            args = args || {};
+            var defaults = {
+              dataType : 'json',
+              contentType : 'application/json; charset=utf-8',
+              dataFilter : corbel.Services.addEmptyJson,
+              accessToken : this.driver.config.get('iamToken', {}).accessToken, // @todo: support to oauth token and custom handlers
+              headers : {
+                Accept : 'application/json'
+              },
+              method : corbel.request.method.GET
+            };
+            var params = buildDefaults(args, defaults);
 
-            args.dataType = args.dataType || 'json';
-            args.contentType = args.contentType || 'application/json; charset=utf-8';
-            args.dataFilter = args.dataFilter || corbel.Services.addEmptyJson;
+            //Data
+            params.data = (params.contentType.indexOf('json') !== -1 && typeof params.data === 'object' ? JSON.stringify(params.data) : params.data);
 
-            // Construct url with query string
-            var url = args.url;
-
-            if (!url) {
+            if (!params.url) {
                 throw new Error('You must define an url');
             }
 
-            if (args.query) {
-                url += '?' + args.query;
+            if (params.query) {
+                params.url += '?' + params.query;
             }
-
-            var headers = args.headers || {};
-
-            // @todo: support to oauth token and custom handlers
-            args.accessToken = args.accessToken || this.driver.config.get('iamToken', {}).accessToken;
 
             // Use access access token if exists
-            if (args.accessToken) {
-                headers.Authorization = 'Bearer ' + args.accessToken;
-            }
-            if (args.noRedirect) {
-                headers['No-Redirect'] = true;
+            if (params.accessToken) {
+                params.headers.Authorization = 'Bearer ' + params.accessToken;
             }
 
-            headers.Accept = 'application/json';
-            if (args.Accept) {
-                headers.Accept = args.Accept;
-                args.dataType = undefined; // Accept & dataType are incompatibles
+            if (params.noRedirect) {
+                params.headers['No-Redirect'] = true;
             }
 
-            var params = {
-                url: url,
-                dataType: args.dataType,
-                contentType: args.contentType,
-                method: args.method || corbel.request.method.GET,
-                headers: headers,
-                data: (args.contentType.indexOf('json') !== -1 && typeof args.data === 'object' ? JSON.stringify(args.data) : args.data),
-                dataFilter: args.dataFilter
-            };
+            if (params.Accept) {
+                params.headers.Accept = params.Accept;
+                params.dataType = undefined; // Accept & dataType are incompatibles
+            }
 
             // For binary requests like 'blob' or 'arraybuffer', set correct dataType
-            params.dataType = args.binaryType || params.dataType;
+            params.dataType = params.binaryType || params.dataType;
 
             // Prevent JQuery to proceess 'blob' || 'arraybuffer' data
             // if ((params.dataType === 'blob' || params.dataType === 'arraybuffer') && (params.method === 'PUT' || params.method === 'POST')) {
@@ -132,7 +181,7 @@ var BaseServices = (function() {
             //      console.log('services._buildParams (data)', args.data);
             // }
 
-            return params;
+            return pick(params, ['url', 'dataType', 'contentType', 'method', 'headers', 'data', 'dataFilter', 'caller']);
         }
     });
 
