7478e9ebe3606ea8bde02fd7bda4bf66f1581a4f
Anthanh Pham Trinh <antai.pt@gmail.com>
2015-09-07 12:18:00 +0200

    EC.order API support chai-as-promised added
    Change-Id: I6979120113fb02d426f5191893f742f085e12d4e

--- src/ec/productBuilder.js
+++ src/ec/productBuilder.js
@@ -4,115 +4,121 @@
 
 (function() {
 
+  /**
+   * Create a ProductBuilder for product managing requests.
+   *
+   * @param {String}  id  The id of the product.
+   *
+   * @return {corbel.Ec.ProductBuilder}
+   */
+  corbel.Ec.prototype.product = function(id) {
+    var product = new ProductBuilder(id);
+    product.driver = this.driver;
+    return product;
+  };
+  /**
+   * A builder for products management requests.
+   *
+   * @param {String}  id product ID.
+   *
+   * @class
+   * @memberOf corbel.Ec.ProductBuilder
+   */
+  var ProductBuilder = corbel.Ec.ProductBuilder = corbel.Services.inherit({
+
+    constructor: function(id) {
+      if (id) {
+        this.id = id;
+      }
+      this.uri = 'product';
+    },
+
     /**
-     * Create a ProductBuilder for product managing requests.
+     * Create a new product.
      *
-     * @param {String}  id  The id of the product.
-     *
-     * @return {corbel.Ec.ProductBuilder}
-     */
-    corbel.Ec.prototype.product = function(id) {
-        var product = new ProductBuilder(id);
-        product.driver = this.driver;
-        return product;
-    };
-    /**
-     * A builder for products management requests.
-     *
-     * @param {String}  id product ID.
-     *
-     * @class
+     * @method
      * @memberOf corbel.Ec.ProductBuilder
+     * @param {Object} product                 Contains the data of the new product
+     * @param {String} product.price           Information about price
+     * @param {String} product.price.currency  Currency code fro the price
+     * @param {Number} product.price.amount    The amount of the price
+     * @param {String} href                 Link to ???
+     * @param {String} type                 Type of the Product
+     * @param {Object} [scopes]             Set of scopes of the product
+     * @param {String} [period]             Duration of the Product
+     * @param {Number} period.years
+     * @param {Number} period.months
+     * @param {Number} period.days
+     * @param {String} [name]               Name of the Product
+     * @return {Promise} A promise with the id of the created loanable resources or fails
+     *                   with a {@link corbelError}.
      */
-    var ProductBuilder = corbel.Ec.ProductBuilder = corbel.Services.inherit({
+    create: function(product) {
+      console.log('ecInterface.product.create', product);
+      return this.request({
+        url: this._buildUri(this.uri),
+        method: corbel.request.method.POST,
+        data: product,
+      }).then(function(res) {
+        return corbel.Services.getLocationId(res);
+      });
+    },
 
-        constructor: function(id) {
-            if (id) {
-                this.id = id;
-            }
-            this.uri = 'product';
-        },
-        /**
-         * Create a new product.
-         *
-         * @method
-         * @memberOf corbel.Ec.ProductBuilder
-         * @param {Object} product                 Contains the data of the new product
-         * @param {String} product.price           Information about price
-         * @param {String} product.price.currency  Currency code fro the price
-         * @param {Number} product.price.amount    The amount of the price
-         * @param {String} href                 Link to ???
-         * @param {String} type                 Type of the Product
-         * @param {Object} [scopes]             Set of scopes of the product
-         * @param {String} [period]             Duration of the Product
-         * @param {Number} period.years
-         * @param {Number} period.months
-         * @param {Number} period.days
-         * @param {String} [name]               Name of the Product
-         * @return {Promise} A promise with the id of the created loanable resources or fails
-         *                   with a {@link corbelError}.
-         */
-        create: function(product) {
-            console.log('ecInterface.product.create', product);
-            return this.request({
-                url: this._buildUri(this.uri),
-                method: corbel.request.method.POST,
-                data: product,
-            }).then(function(res) {
-                return corbel.Services.getLocationId(res);
-            });
-        },
-        /**
-         * Get a product.
-         *
-         * @method
-         * @memberOf corbel.Ec.EcBuilder
-         *
-         * @param  {Object} params  The params filter
-         * 
-         * @return {Promise} A promise with product {Object} or fails with a {@link corbelError}.
-         */
-        get: function(params) {
-            console.log('ecInterface.product.get');
-            return this.request({
-                url: this._buildUri(this.uri ,this.id),
-                method: corbel.request.method.GET,
-                query: params ? corbel.utils.serializeParams(params) : null
-            });
-        },
-        /**
-         * Update a product.
-         *
-         * @method
-         * @memberOf corbel.Ec.EcBuilder
-         *
-         * @param  {Object} product  The product update
-         * 
-         * @return {Promise} A promise resolves to undefined (void) or fails with a {@link corbelError}.
-         */
-        update: function(product) {
-            console.log('ecInterface.product.update');
-            return this.request({
-                url: this._buildUri(this.uri, this.id),
-                method: corbel.request.method.PUT,
-                data: product
-            });
-        },
-        /**Delete a product.
-         *
-         * @method
-         * @memberOf corbel.Ec.EcBuilder
-         *
-         * @return {Promise} A promise resolves to undefined (void) or fails with a {@link corbelError}.
-         */
-        delete: function() {
-            console.log('ecInterface.product.delete');
-            return this.request({
-                url: this._buildUri(this.uri, this.id),
-                method: corbel.request.method.DELETE
-            });
-        },
+    /**
+     * Get a product.
+     *
+     * @method
+     * @memberOf corbel.Ec.EcBuilder
+     *
+     * @param  {Object} params  The params filter
+     * 
+     * @return {Promise} A promise with product {Object} or fails with a {@link corbelError}.
+     */
+    get: function(params) {
+      console.log('ecInterface.product.get');
+      return this.request({
+        url: this._buildUri(this.uri, this.id),
+        method: corbel.request.method.GET,
+        query: params ? corbel.utils.serializeParams(params) : null
+      });
+    },
 
-        _buildUri: corbel.Ec._buildUri
-    });
+    /**
+     * Update a product.
+     *
+     * @method
+     * @memberOf corbel.Ec.EcBuilder
+     *
+     * @param  {Object} product  The product update
+     * 
+     * @return {Promise} A promise resolves to undefined (void) or fails with a {@link corbelError}.
+     */
+    update: function(product) {
+      console.log('ecInterface.product.update');
+      return this.request({
+        url: this._buildUri(this.uri, this.id),
+        method: corbel.request.method.PUT,
+        data: product
+      });
+    },
+
+    /**Delete a product.
+     *
+     * @method
+     * @memberOf corbel.Ec.EcBuilder
+     *
+     * @return {Promise} A promise resolves to undefined (void) or fails with a {@link corbelError}.
+     */
+    delete: function() {
+      console.log('ecInterface.product.delete');
+      return this.request({
+        url: this._buildUri(this.uri, this.id),
+        method: corbel.request.method.DELETE
+      });
+    },
+
+    _buildUri: corbel.Ec._buildUri
+
+  });
+
 })();
--- test/node/unit/ec.js
+++ test/node/unit/ec.js
@@ -1,108 +1,209 @@
 'use strict';
 
 var corbel = require('../../../dist/corbel.js'),
-    chai = require('chai'),
-    sinon = require('sinon'),
-    expect = chai.expect;
+  chai = require('chai'),
+  sinon = require('sinon'),
+  expect = chai.expect;
 
 describe('In EC module', function() {
 
-    var sandbox = sinon.sandbox.create();
-    var CONFIG = {
+  var sandbox = sinon.sandbox.create();
+  var CONFIG = {
 
-        clientId: 'clientId',
-        clientSecret: 'clientSecret',
+    clientId: 'clientId',
+    clientSecret: 'clientSecret',
 
-        scopes: ['silkroad-qa:client', 'resources:send_event_bus', 'resources:test:test_operations', 'resources:music:read_catalog', 'resources:music:streaming'],
+    scopes: ['silkroad-qa:client', 'resources:send_event_bus', 'resources:test:test_operations', 'resources:music:read_catalog', 'resources:music:streaming'],
 
-        urlBase: 'https://{{module}}-corbel.io/'
+    urlBase: 'https://{{module}}-corbel.io/'
 
-    };
+  };
 
-    var PRODUCT_EC_URL = CONFIG.urlBase.replace('{{module}}', 'ec') + 'product';
+  var EC_URL = CONFIG.urlBase.replace('{{module}}', corbel.Ec.moduleName);
 
-    var corbelDriver = corbel.getDriver(CONFIG);
+  var corbelDriver = corbel.getDriver(CONFIG);
 
-    var corbelRequestStub;
+  var corbelRequestStub;
 
-    beforeEach(function() {
-        corbelRequestStub = sandbox.stub(corbel.request, 'send');
+  beforeEach(function() {
+    corbelRequestStub = sandbox.stub(corbel.request, 'send');
+  });
+
+  afterEach(function() {
+    sandbox.restore();
+  });
+
+  describe('with product,', function() {
+
+    it('create one', function() {
+      corbelRequestStub.returns(Promise.resolve());
+      var productData = '{\'test_object\':\'test\'}';
+      corbelDriver.ec.product().create(productData);
+
+      var callRequestParam = corbelRequestStub.getCall(0).args[0];
+      expect(callRequestParam.url).to.be.equal(EC_URL + 'product');
+      expect(callRequestParam.method).to.be.equal('POST');
+      expect(callRequestParam.data).to.be.equal(productData);
     });
 
-    afterEach(function() {
-        sandbox.restore();
+    it('get one', function() {
+      corbelRequestStub.returns(Promise.resolve('OK'));
+
+      corbelDriver.ec.product('idProduct').get();
+
+      var callRequestParam = corbel.request.send.firstCall.args[0];
+      expect(callRequestParam.url).to.be.include(EC_URL + 'product');
+      expect(callRequestParam.method).to.be.equal('GET');
     });
-    describe('with product,', function() {
-        it('create one', function() {
-            corbelRequestStub.returns(Promise.resolve());
-            var productData = '{\'test_object\':\'test\'}';
-            corbelDriver.ec.product().create(productData);
 
-            var callRequestParam = corbelRequestStub.getCall(0).args[0];
-            expect(callRequestParam.url).to.be.equal(PRODUCT_EC_URL);
-            expect(callRequestParam.method).to.be.equal('POST');
-            expect(callRequestParam.data).to.be.equal(productData);
-        });
+    it('get all with params', function() {
+      corbelRequestStub.returns(Promise.resolve('OK'));
+      var params = {
+        query: [{
+          '$eq': {
+            field: 'value'
+          }
+        }],
+        pagination: {
+          pageSize: 2,
+          page: 3
+        },
+        sort: {
+          field: 'asc'
+        }
+      };
 
-        it('get one', function() {
-            corbelRequestStub.returns(Promise.resolve('OK'));
+      corbelDriver.ec.product().get(params);
 
-            corbelDriver.ec.product('idProduct').get();
-
-            var callRequestParam = corbel.request.send.firstCall.args[0];
-            expect(callRequestParam.url).to.be.include(PRODUCT_EC_URL);
-            expect(callRequestParam.method).to.be.equal('GET');
-        });
-
-        it('get all with params', function() {
-            corbelRequestStub.returns(Promise.resolve('OK'));
-            var params = {
-                query: [{
-                    '$eq': {
-                        field: 'value'
-                    }
-                }],
-                pagination: {
-                    pageSize: 2,
-                    page: 3
-                },
-                sort: {
-                    field: 'asc'
-                }
-            };
-
-            corbelDriver.ec.product().get(params);
-
-            var callRequestParam = corbel.request.send.firstCall.args[0];
-            expect(callRequestParam.url).to.be.include(PRODUCT_EC_URL);
-            expect(callRequestParam.method).to.be.equal('GET');
-            console.log(callRequestParam);
-            expect(callRequestParam.url).to.contain('api:query=[{"$eq":{"field":"value"}}]&api:sort={"field":"asc"}&api:page=3&api:pageSize=2');
-        });
-
-        it('update one', function() {
-            corbelRequestStub.returns(Promise.resolve(204));
-            var productDataUpdate = '{\'test_object\':\'testUpdate\'}';
-            var idProduct = 1;
-
-            corbelDriver.ec.product(idProduct).update(productDataUpdate);
-
-            var callRequestParam = corbel.request.send.firstCall.args[0];
-            expect(callRequestParam.url).to.be.equal(PRODUCT_EC_URL + '/1');
-            expect(callRequestParam.method).to.be.equal('PUT');
-            expect(callRequestParam.data).to.be.equal(productDataUpdate);
-
-        });
-
-        it('delete one', function() {
-            corbelRequestStub.returns(Promise.resolve(204));
-            var idProduct = 1;
-
-            corbelDriver.ec.product(idProduct).delete();
-
-            var callRequestParam = corbel.request.send.firstCall.args[0];
-            expect(callRequestParam.url).to.be.equal(PRODUCT_EC_URL + '/1');
-            expect(callRequestParam.method).to.be.equal('DELETE');
-        });
+      var callRequestParam = corbel.request.send.firstCall.args[0];
+      expect(callRequestParam.url).to.be.include(EC_URL + 'product');
+      expect(callRequestParam.method).to.be.equal('GET');
+      console.log(callRequestParam);
+      expect(callRequestParam.url).to.contain('api:query=[{"$eq":{"field":"value"}}]&api:sort={"field":"asc"}&api:page=3&api:pageSize=2');
     });
+
+    it('update one', function() {
+      corbelRequestStub.returns(Promise.resolve(204));
+      var productDataUpdate = '{\'test_object\':\'testUpdate\'}';
+      var idProduct = 1;
+
+      corbelDriver.ec.product(idProduct).update(productDataUpdate);
+
+      var callRequestParam = corbel.request.send.firstCall.args[0];
+      expect(callRequestParam.url).to.be.equal(EC_URL + 'product' + '/1');
+      expect(callRequestParam.method).to.be.equal('PUT');
+      expect(callRequestParam.data).to.be.equal(productDataUpdate);
+    });
+
+    it('delete one', function() {
+      corbelRequestStub.returns(Promise.resolve(204));
+      var idProduct = 1;
+
+      corbelDriver.ec.product(idProduct).delete();
+
+      var callRequestParam = corbel.request.send.firstCall.args[0];
+      expect(callRequestParam.url).to.be.equal(EC_URL + 'product' + '/1');
+      expect(callRequestParam.method).to.be.equal('DELETE');
+    });
+
+  });
+
+  describe('with order,', function() {
+
+    it('get one', function(done) {
+      corbelRequestStub.returns(Promise.resolve('ok'));
+      var idOrder = 1;
+      var response = corbelDriver.ec.order(idOrder).get();
+      expect(response).be.eventually.fulfilled
+        .then(function(response) {
+          expect(response).to.be.equal('ok');
+
+          var paramsRecived = corbel.request.send.firstCall.args[0];
+          expect(paramsRecived.url).to.be.equal(EC_URL + 'order/1');
+          expect(paramsRecived.method).to.be.equal('GET');
+        }).should.be.eventually.fulfilled
+        .and.notify(done);
+
+    });
+
+    it('update one', function(done) {
+      corbelRequestStub.returns(Promise.resolve(204));
+      var productDataUpdate = '{\'test_object\':\'testUpdate\'}';
+      var idOrder = 1;
+
+      var response = corbelDriver.ec.order(idOrder).update(productDataUpdate);
+      expect(response).be.eventually.fulfilled
+        .then(function(response) {
+          expect(response).to.be.equal(204);
+
+          var paramsRecived = corbel.request.send.firstCall.args[0];
+          expect(paramsRecived.url).to.be.equal(EC_URL + 'order/1');
+          expect(paramsRecived.method).to.be.equal('PUT');
+          expect(paramsRecived.data).to.be.equal(productDataUpdate);
+        }).should.be.eventually.fulfilled
+        .and.notify(done);
+
+    });
+
+    it('delete one', function(done) {
+      corbelRequestStub.returns(Promise.resolve(204));
+      var idOrder = 1;
+
+      var response = corbelDriver.ec.order(idOrder).delete();
+      expect(response).be.eventually.fulfilled
+        .then(function(response) {
+          expect(response).to.be.equal(204);
+
+          var paramsRecived = corbel.request.send.firstCall.args[0];
+          expect(paramsRecived.url).to.be.equal(EC_URL + 'order/1');
+          expect(paramsRecived.method).to.be.equal('DELETE');
+        }).should.be.eventually.fulfilled
+        .and.notify(done);
+
+    });
+
+    it('checkout', function() {
+      corbelRequestStub.returns(Promise.resolve());
+      var checkoutData = {
+        paymentMethodIds: ['paymentMethod']
+      };
+      corbelDriver.ec.order('orderId').checkout(checkoutData);
+
+      var paramsRecived = corbel.request.send.firstCall.args[0];
+      expect(paramsRecived.url).to.be.equal(EC_URL + 'order/orderId/checkout');
+      expect(paramsRecived.method).to.be.equal('POST');
+      expect(paramsRecived.data).to.be.equal(checkoutData);
+    });
+
+    it('checkout failed with empty payment methods', function(done) {
+      corbelRequestStub.returns(Promise.resolve(202));
+      var checkoutData = {
+        paymentMethodIds: []
+      };
+
+      expect(corbelDriver.ec.order('orderId').checkout(checkoutData)).be.eventually.rejected
+        .then(function(response) {
+          expect(response.message).to.be.equal('One payment method is needed at least');
+        }).should.be.eventually.fulfilled
+        .and.notify(done);
+    });
+
+    it('prepare', function(done) {
+      corbelRequestStub.returns(Promise.resolve(200));
+
+      var response = corbelDriver.ec.order('orderId').prepare();
+      expect(response).be.eventually.fulfilled
+        .then(function(response) {
+          expect(response).to.be.equal(200);
+
+          var paramsRecived = corbel.request.send.firstCall.args[0];
+          expect(paramsRecived.url).to.be.equal(EC_URL + 'order/orderId/prepare');
+          expect(paramsRecived.method).to.be.equal('POST');
+        }).should.be.eventually.fulfilled
+        .and.notify(done);
+
+    });
+
+  });
+
 });
