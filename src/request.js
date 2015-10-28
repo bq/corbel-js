9566c760b1460ca8fab35635b3094b8e07a89cf5
Ivan Rodriguez <ivan.rodriguez@bq.com>
2015-10-28 15:59:15 +0100

    Add requests throw error when disconnected
    Change-Id: I8c5298c0547fd0dae8b440379420a69f70b5b00a

--- src/request.js
+++ src/request.js
@@ -254,7 +254,6 @@
 
   var xhrSuccessStatus = {
     // file protocol always yields status code 0, assume 200
-    0: 200,
     // Support: IE9
     // #1450: sometimes IE returns 1223 when it should be 204
     1223: 204
@@ -276,47 +275,47 @@
 
     var data = response.response;
 
-    if (statusType < 3) {
+    if (statusType === 4 || response.error) {
 
-      if (response.response) {
+        if (callbackError) {
+          callbackError.call(this, response.error, statusCode, response.responseObject, response.headers);
+        }
+
+        if (response.response) {
+          data = request.parse(response.response, response.responseType, response.dataType);
+        }
+
+        promiseResponse = {
+          data: data,
+          status: statusCode,
+          error: response.error,
+          headers: response.headers
+        };
+
+        promiseResponse[response.responseObjectType] = response.responseObject;
+
+        resolver.reject(promiseResponse);
+
+    } else if (statusType < 3) {
+
+        if (response.response) {
         data = request.parse(response.response, response.responseType, response.dataType);
-      }
+        }
 
-      if (callbackSuccess) {
+        if (callbackSuccess) {
         callbackSuccess.call(this, data, statusCode, response.responseObject, response.headers);
-      }
+        }
 
-      promiseResponse = {
+        promiseResponse = {
         data: data,
         status: statusCode,
         headers: response.headers
-      };
+        };
 
-      promiseResponse[response.responseObjectType] = response.responseObject;
+        promiseResponse[response.responseObjectType] = response.responseObject;
 
-      resolver.resolve(promiseResponse);
-
-    } else if (statusType === 4) {
-
-      if (callbackError) {
-        callbackError.call(this, response.error, statusCode, response.responseObject, response.headers);
+        resolver.resolve(promiseResponse);
       }
-
-      if (response.response) {
-        data = request.parse(response.response, response.responseType, response.dataType);
-      }
-
-      promiseResponse = {
-        data: data,
-        status: statusCode,
-        error: response.error,
-        headers: response.headers
-      };
-
-      promiseResponse[response.responseObjectType] = response.responseObject;
-
-      resolver.reject(promiseResponse);
-    }
 
   };
 
@@ -465,6 +464,13 @@
     httpReq.onerror = function(xhr) {
       xhr = xhr.target || xhr; // only for fake sinon response xhr
 
+      var error;
+
+      // Error flag to support disconnection errors
+      if (xhr.type === 'error') {
+          error = true;
+      }
+
       processResponse.call(this, {
         responseObject: xhr,
         dataType: xhr.dataType,
@@ -472,7 +478,7 @@
         response: xhr.response || xhr.responseText,
         status: xhr.status,
         responseObjectType: 'xhr',
-        error: xhr.error
+        error: xhr.error || error
       }, resolver, params.callbackSuccess, params.callbackError);
 
     }.bind(this);
