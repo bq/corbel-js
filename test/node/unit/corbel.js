1ae3ad6e9da8fcfa2bfc7547d8ae328dbbd7572f
Anthanh Pham Trinh <antai.pt@gmail.com>
2015-11-19 11:29:25 +0100

    corbel driver with pub/sub support
    Change-Id: I6c731b80dd8e9448c0adac53fd69b20d12f390d3

--- test/node/unit/corbel.js
+++ test/node/unit/corbel.js
@@ -2,9 +2,31 @@
 
 var corbel = require('../../../dist/corbel.js'),
   chai = require('chai'),
+  sinon = require('sinon'),
   expect = chai.expect;
 
 describe('in corbel module', function() {
+
+  var sandbox = sinon.sandbox.create();
+  var corbelDriver;
+
+  function createDriver() {
+    return corbel.getDriver({
+      clientId: 'clientId',
+      clientSecret: 'clientSecret',
+      scopes: 'scopesApp',
+
+      urlBase: 'https://{{module}}-qa.bqws.io/v1.0/'
+    });
+  }
+
+  beforeEach(function() {
+    corbelDriver = createDriver();
+  });
+
+  afterEach(function() {
+    sandbox.restore();
+  });
 
   it('is defined and is an object', function() {
     expect(corbel).to.be.an('object');
@@ -36,30 +58,12 @@ describe('in corbel module', function() {
     describe('with all parametes', function() {
 
       it('it creates a CorbelDriver', function() {
-        expect(corbel.getDriver({
-          clientId: 'clientId',
-          clientSecret: 'clientSecret',
-          scopes: 'scopesApp',
-
-          urlBase: 'https://{{module}}-qa.bqws.io/v1.0/'
-        })).to.be.an.instanceof(corbel.CorbelDriver);
+        expect(corbelDriver).to.be.an.instanceof(corbel.CorbelDriver);
       });
 
     });
 
     describe('with a CorbelDriver instance', function() {
-
-      var corbelDriver;
-
-      beforeEach(function() {
-        corbelDriver = corbel.getDriver({
-          clientId: 'clientId',
-          clientSecret: 'clientSecret',
-          scopes: 'scopesApp',
-
-          urlBase: 'https://{{module}}-qa.bqws.io/v1.0/'
-        });
-      });
 
       it('has all members', function() {
         expect(corbelDriver).to.have.ownProperty('config');
@@ -75,14 +79,6 @@ describe('in corbel module', function() {
 
   it('can clone a new driver with the same config', function() {
 
-    var corbelDriver = corbel.getDriver({
-      clientId: 'clientId',
-      clientSecret: 'clientSecret',
-      scopes: 'scopesApp',
-
-      urlBase: 'https://{{module}}-qa.bqws.io/v1.0/'
-    });
-
     var clonedDriver = corbelDriver.clone();
     expect(clonedDriver !== corbelDriver).to.be.equal(true);
 
@@ -94,6 +90,152 @@ describe('in corbel module', function() {
     expect(driverConfig.clientSecret).to.be.equal(clonedConfig.clientSecret);
     expect(driverConfig.scopes).to.be.equal(clonedConfig.scopes);
     expect(driverConfig.urlBase).to.be.equal(clonedConfig.urlBase);
+
+  });
+
+  describe('Event system', function() {
+
+    it('can register new handler', function() {
+
+      var stub = sandbox.stub();
+
+      expect(corbelDriver._events['custom:event:name']).to.be.equal(undefined);
+      corbelDriver.addEventListener('custom:event:name', stub);
+
+      expect(corbelDriver._events['custom:event:name'].length).to.be.equal(1);
+
+      corbelDriver.dispatch('custom:event:name', {
+        params: true
+      });
+
+      expect(stub.calledOnce).to.be.equal(true);
+      expect(stub.getCall(0).args[0]).to.be.an('object');
+      expect(stub.getCall(0).args[0].params).to.be.equal(true);
+
+    });
+
+    it('register same handler once', function() {
+
+      var stub = sandbox.stub();
+
+      expect(corbelDriver._events['custom:event:name']).to.be.equal(undefined);
+      corbelDriver.addEventListener('custom:event:name', stub);
+      corbelDriver.addEventListener('custom:event:name', stub);
+      corbelDriver.addEventListener('custom:event:name', stub);
+
+      expect(corbelDriver._events['custom:event:name'].length).to.be.equal(1);
+
+      corbelDriver.dispatch('custom:event:name', {
+        params: true
+      });
+
+      expect(stub.calledOnce).to.be.equal(true);
+      expect(stub.getCall(0).args[0]).to.be.an('object');
+      expect(stub.getCall(0).args[0].params).to.be.equal(true);
+
+    });
+
+    it('can remove a handler', function() {
+
+      var stub = sandbox.stub();
+
+      expect(corbelDriver._events['custom:event:name']).to.be.equal(undefined);
+      corbelDriver.addEventListener('custom:event:name', stub);
+      corbelDriver.removeEventListener('custom:event:name', stub);
+
+      expect(corbelDriver._events['custom:event:name'].length).to.be.equal(0);
+
+      corbelDriver.dispatch('custom:event:name', {
+        params: true
+      });
+
+      expect(stub.callCount).to.be.equal(0);
+
+    });
+
+    it('can remove a non existing handler', function() {
+
+      var stub = sandbox.stub();
+
+      expect(corbelDriver._events['custom:event:name']).to.be.equal(undefined);
+      corbelDriver.removeEventListener('custom:event:name', stub);
+
+      expect(corbelDriver._events['custom:event:name']).to.be.equal(undefined);
+
+    });
+
+    describe('trigger an event', function() {
+
+      it('fires handlers as expected', function() {
+
+        var stub = sandbox.stub();
+
+        expect(corbelDriver._events['custom:event:name']).to.be.equal(undefined);
+        corbelDriver.addEventListener('custom:event:name', stub);
+
+        expect(corbelDriver._events['custom:event:name'].length).to.be.equal(1);
+
+        corbelDriver.dispatch('custom:event:name', {
+          params: true
+        });
+
+        expect(stub.calledOnce).to.be.equal(true);
+        expect(stub.getCall(0).args[0]).to.be.an('object');
+        expect(stub.getCall(0).args[0].params).to.be.equal(true);
+
+      });
+
+      it('fires handlers only once', function() {
+
+        var stub = sandbox.stub();
+        var stub2 = sandbox.stub();
+
+        expect(corbelDriver._events['custom:event:name']).to.be.equal(undefined);
+        corbelDriver.addEventListener('custom:event:name', stub);
+        corbelDriver.addEventListener('custom:event:name', stub);
+        corbelDriver.addEventListener('custom:event:name', stub2);
+        corbelDriver.addEventListener('custom:event:name', stub2);
+        corbelDriver.removeEventListener('custom:event:name', stub);
+        corbelDriver.addEventListener('custom:event:name', stub);
+
+        expect(corbelDriver._events['custom:event:name'].length).to.be.equal(2);
+
+        corbelDriver.dispatch('custom:event:name', {
+          params: true
+        });
+
+        expect(stub.calledOnce).to.be.equal(true);
+        expect(stub2.calledOnce).to.be.equal(true);
+        expect(stub.getCall(0).args[0]).to.be.an('object');
+        expect(stub.getCall(0).args[0].params).to.be.equal(true);
+
+        expect(stub2.getCall(0).args[0]).to.be.an('object');
+        expect(stub2.getCall(0).args[0].params).to.be.equal(true);
+
+      });
+
+      it('do not fires removed handlers', function() {
+
+        var stub = sandbox.stub();
+        var stub2 = sandbox.stub();
+
+        expect(corbelDriver._events['custom:event:name']).to.be.equal(undefined);
+        corbelDriver.addEventListener('custom:event:name', stub);
+        corbelDriver.addEventListener('custom:event:name', stub2);
+        corbelDriver.removeEventListener('custom:event:name', stub);
+
+        expect(corbelDriver._events['custom:event:name'].length).to.be.equal(1);
+
+        corbelDriver.dispatch('custom:event:name', {
+          params: true
+        });
+
+        expect(stub.callCount).to.be.equal(0);
+        expect(stub2.callCount).to.be.equal(1);
+
+      });
+
+    });
 
   });
 
