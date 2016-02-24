2d9221a1b1d1c341fc64a26c42ec34ace62b93b7
David Garcia <barduk91@gmail.com>
2016-02-24 15:52:43 +0100

    Add endpoint PaymentPlan

--- src/ec/paymentPlanBuilder.js
+++ src/ec/paymentPlanBuilder.js
@@ -9,7 +9,7 @@
    *
    * @return {corbel.Ec.PaymentPlanBuilder}
    */
-  corbel.Ec.prototype.payment = function () {
+  corbel.Ec.prototype.paymentPlan = function () {
     var paymentPlan = new PaymentPlantBuilder();
     paymentPlan.driver = this.driver;
     return paymentPlan;
@@ -32,13 +32,12 @@
      * @class
      * @memberOf corbel.Ec.PaymentPlanBuilder
      */
-    add: function (params) {
+    get: function (params) {
       console.log('ecInterface.paymentplan.add');
 
       return this.request({
         url: this._buildUri(this.uri),
         method: corbel.request.method.POST,
-        data: params
       });
     },
 
@@ -47,6 +46,11 @@
      *
      * @class
      * @memberOf corbel.Ec.PaymentPlanBuilder
+     *
+     * @param {String} id                Payment method identifier
+     *
+     * @return {Promise}                 Q promise that resolves to a Payment {Object} or rejects with a
+     *                                   {@link SilkRoadError}
      */
      getById: function (id) {
       console.log('ecInterface.paymentplan.get');
@@ -58,6 +62,90 @@
       });
     },
 
+    /**
+     * Deletes a payment plan.
+     *
+     * @method
+     * @memberOf corbel.Ec.PaymentPlanBuilder
+     *
+     * @param {String} id                Payment method identifier
+     *
+     * @return {Promise}                 Q promise that resolves to a Payment {Object} or rejects with a
+     *                                   {@link SilkRoadError}
+     */
+    delete: function (id) {
+      console.log('ecInterface.paymentplan.delete');
+
+      corbel.validate.value('id', id);
+      return this.request({
+        url: this._buildUri(this.uri, id),
+        method: corbel.request.method.DELETE
+      });
+    },
+
+    /**
+     * Change the payment plan status from terminated to open (reactivated the payment plan)
+     * 
+     * @method
+     * @memberOf corbel.Ec.PaymentPlanBuilder
+     *
+     * @param {String} id                Payment method identifier
+     * 
+     */
+     rescue: function(){
+      console.log('ecInterface.paymentplan.rescue');
+
+      corbel.validate.value('id', this.id);
+      return this.request({
+        url: this._buildUri(this.uri, this.id),
+        method: corbel.request.method.PUT,
+        data: product
+      });
+     },
+
+     /**
+     * Change the payment method of a playment plan
+     * 
+     * @method
+     * @memberOf corbel.Ec.PaymentPlanBuilder
+     *
+     * @param {Object} product                      The product update
+     * @param {String} product.paymentMethodId      Identifier of the payment method to use with the plan
+     *
+     */
+     update: function(){
+      console.log('ecInterface.paymentplan.update');
+
+      corbel.validate.value('id', this.id);
+      return this.request({
+        url: this._buildUri(this.uri, this.id),
+        method: corbel.request.method.PUT,
+        data: product
+     });
+     },
+
+     /**
+     * Gets payment plans paginated, this endpoint is only for admins
+     *
+     * @method
+     * @memberOf corbel.Ec.PaymentPlanBuilder
+     * @param {Object} params                 The params filter
+     * @param {Integer} params.api:pageSize   Number of result returned in the page (>0 , default: 10)
+     * @param {String} params.api:query       A search query expressed in silkroad query language
+     * @param {Integer} params.api:page       The page to be returned. Pages are zero-indexed (>0, default:0)
+     * @param {String} params.api:sort        Results orders. JSON with field to order and direction, asc or desc
+     *
+     * @return {Promise}        Q promise that resolves to a Payment {Object} or rejects with a {@link SilkRoadError}
+     */
+    getAll: function (params) {
+      console.log('ecInterface.paymentplan.getAll');
+
+      return this.request({
+        url: this._buildUri(this.uri, 'all'),
+        method: corbel.request.method.GET,
+        query: params ? corbel.utils.serializeParams(params) : null
+      });
+    },
 
     /**
      * Internal module uri builder
