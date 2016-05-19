829fcbdbdbcd2a97a5b9f8c6f0efe34e473492e4
Francisco Sanchez <francisco.sanchez@bq.com>
2016-05-19 15:39:38 +0200

    update dist file

--- dist/corbel.with-polyfills.js
+++ dist/corbel.with-polyfills.js
@@ -5546,9 +5546,10 @@
 
                 var args = corbel.utils.extend(options, {
                     url: this.buildUri(this.type, this.srcId, this.destType, destId),
-                    contentType: 'application/json',
                     data: relationData,
-                    method: corbel.request.method.PUT
+                    method: corbel.request.method.PUT,
+                    contentType: options.dataType,
+                    Accept: options.dataType
                 });
 
                 return this.request(args);
@@ -5567,9 +5568,10 @@
 
                 var args = corbel.utils.extend(options, {
                     url: this.buildUri(this.type, this.srcId, this.destType),
-                    contentType: 'application/json',
                     data: relationData,
-                    method: corbel.request.method.POST
+                    method: corbel.request.method.POST,
+                    contentType: options.dataType,
+                    Accept: options.dataType
                 });
 
                 return this.request(args);
@@ -5617,7 +5619,8 @@
 
                 var args = corbel.utils.extend(options, {
                     url: this.buildUri(this.type, this.srcId, this.destType, destId),
-                    method: corbel.request.method.DELETE
+                    method: corbel.request.method.DELETE,
+                    Accept: options.dataType
                 });
 
                 return this.request(args);
